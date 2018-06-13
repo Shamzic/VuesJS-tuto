f4ac419875cceb267e8fd2967beca49bd864c5ca
shamzic <shamery@free.fr>
2018-06-14 00:22:08 +0200

    Tuto vues js #5 done

---
+++ app.js
@@ -0,0 +1,27 @@
+new Vue({
+  el: '#vue-app',
+  data: {
+    name: 'Shaun',
+    job: 'Ninja',
+    website: 'http://simonhamery.com',
+    websiteTag: '<a href="http://simonhamery.com"> The Net Ninja website </a>',
+    age: 25,
+    x: 0,
+    y: 0
+  },
+  methods: {
+    greet: function(time){
+      return 'Good ' + time + ' '+ this.name;
+    },
+    add: function(n) {
+      this.age+=n;
+    },
+    sub: function(n) {
+      this.age-=n;
+    },
+    updateXY: function(event) {
+      this.x = event.offsetX;
+      this.y = event.offsetY;
+    }
+  }
+});
