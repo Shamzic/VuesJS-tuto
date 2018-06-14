7f7d1ffe712c8ff52a89b8e42b2c040a09ae411a
shamzic <shamery@free.fr>
2018-06-15 01:47:57 +0200

    Tutp vueJS #10 done

--- app.js
+++ app.js
@@ -1,53 +1,18 @@
 new Vue({
   el: '#vue-app',
   data: {
-    name: '',
-    job: 'Ninja',
-    website: 'http://simonhamery.com',
-    websiteTag: '<a href="http://simonhamery.com"> The Net Ninja website </a>',
-    age: 20,
-    x: 0,
-    y: 0,
-    a: 0,
-    b: 0
+    available: false,
+    nearby: false
   },
   methods: {
-    greet: function(time){
-      return 'Good ' + time + ' '+ this.name;
-    },
-    add: function(n) {
-      this.age+=n;
-    },
-    sub: function(n) {
-      this.age-=n;
-    },
-    updateXY: function(event) {
-      this.x = event.offsetX;
-      this.y = event.offsetY;
-    },
-    clickprev: function() {
-      alert('YO');
-    },
-    logName: function() {
-      console.log("you entered your name");
-    },
-    logAge: function() {
-      console.log("you entered your age");
-    }
-    // ,
-    // addToA: function() {
-    //     return this.a+ this.age
-    // },
-    // addToB: function() {
-    //     return this.b+ this.age
-    // }
+
   },
   computed: {
-    addToA: function() {
-        return this.a+ this.age
-    },
-    addToB: function() {
-        return this.b+ this.age
-    },
+    compClasses: function () {
+      return {
+        available: this.available,
+        nearby: this.nearby
+      }
+    }
   }
 });
